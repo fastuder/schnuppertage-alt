6ed84e200eefe4c83528f567e5e643359f026082
Lian Studer <lian.studer@hslu.ch>
2022-12-13 16:16:01 +0100

    add comments for explanation in js

--- src/script.js
+++ src/script.js
@@ -1,3 +1,4 @@
+// Ignorieren, das sind einige Helfer, welche das Ganze etwas einfacher machen
 import {
   receiveMessage,
   sendMessage,
@@ -6,26 +7,40 @@ import {
   scrollToBottom,
 } from "./framework.js";
 
+// Verbindung zum Chat Server herstellen.
 const socket = io(
   "http://hslu-schnuppertage-chat-socket-l8dci.ondigitalocean.app:443"
 );
 
+// -- Nachricht empfangen
+// Auf eine neue Nachricht vom Server warten. Bei einer neuen Nachricht den inneren Teil
+// dieses Abschnitts ausführen.
 socket.on("message", (msg) => {
+  // Nachricht im Chat anzeigen
   receiveMessage(msg);
 });
 
-socket.on("new-connection", (msg) => {
-  console.log(msg);
-});
-
+// -- Nachricht senden
+// Auf Absenden des Formulars zum Senden einer Nachricht warten. Beim Absenden den inneren Teil
+// dieses Abschnitts ausführen
 messageForm.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  // Nachricht aus dem Eingabefeld auslesen
   const message = messageInput.value;
+
+  // Prüfen, ob die Eingabe gültig ist
   if (message.trim().length === 0) return;
 
+  // Nachricht an den Server senden
   socket.emit("message", message);
+
+  // Nachricht im Chat anzeigen
   sendMessage(message);
+
+  // Den Chat automatisch zur neusten Nachricht herunterscrollen
   scrollToBottom();
 
+  // Das Eingabefeld der Nachricht leeren
   messageInput.value = "";
 });
