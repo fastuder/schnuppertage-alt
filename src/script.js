900e627a2656d68816244c235ab91ae215e58c37
Lian Studer <lian.studer@hslu.ch>
2023-03-06 17:55:07 +0100

    finish new template

--- src/script.js
+++ src/script.js
@@ -3,57 +3,56 @@ const messageInput = $("#message-input");
 
 const chatMessageList = $("#chat-messages > ul");
 
-// -- Neues DOM Element erstellen und bearbeiten
+// -- Verbindung zum Chat Server herstellen.
+/*
+    !!! Füge hier deinen Code ein !!!
+*/
+let socket;
+
+// -- Neues DOM Element erstellen und verändern
+// Neues Element erstellen
 const newMessage = (message) => {
-  const messageListItem = $("<li></li>").addClass("chat-message");
-
-  const messageTextElement = $("<span></span>").text(message);
-  messageListItem.append(messageTextElement);
-
-  return messageListItem;
+  /*
+    !!! Füge hier deinen Code ein !!!
+  */
 };
 
-// -- Empfangene Nachricht anzeigen
-const receiveMessage = (message) => {
-  const newElement = newMessage(message);
-  newElement.attr("data-receiver", true);
-
-  chatMessageList.append(newElement);
-};
-
+// -- Neues DOM Element erstellen und verändern
 // -- Gesendete Nachricht anzeigen
 const sendMessage = (message) => {
-  const newElement = newMessage(message);
-  newElement.attr("data-sender", true);
-
-  chatMessageList.append(newElement);
+  /*
+    !!! Füge hier deinen Code ein !!!
+  */
 };
+
+// -- Neues DOM Element erstellen und verändern
+// -- Empfangene Nachricht anzeigen
+const receiveMessage = (message) => {
+  /*
+    !!! Füge hier deinen Code ein !!!
+  */
+};
+
+// -- Nachrichten senden und empfangen
+// Nachricht senden
+messageForm.submit((e) => {
+  e.preventDefault();
+
+  /*
+    !!! Füge hier deinen Code ein !!!
+  */
+});
+
+// -- Nachrichten senden und empfangen
+// Nachricht empfangen
+/*
+  !!! Füge hier deinen Code ein !!!
+*/
 
 // -- Automatisches Scrollen
 // Den Chat automatisch zur neusten Nachricht herunterscrollen
 const scrollToBottom = () => {
-  chatMessageList.scrollTop(chatMessageList.height());
-};
-
-// Verbindung zum Chat Server herstellen.
-/**
-  Aufgabe 2.3: Schreibe hier deinen Code hin!
-*/
-
-// -- Nachricht empfangen
-// Auf eine neue Nachricht vom Server warten. Bei einer neuen Nachricht den inneren Teil
-// dieses Abschnitts ausführen.
-/**
-  Aufgabe 2.4: Schreibe hier deinen Code hin!
-*/
-
-// -- Nachricht senden
-// Auf Absenden des Formulars zum Senden einer Nachricht warten. Beim Absenden den inneren Teil
-// dieses Abschnitts ausführen
-messageForm.submit((e) => {
-  e.preventDefault();
-
-  /**
-    Aufgabe 2.5: Schreibe hier deinen Code hin!
+  /*
+    !!! Füge hier deinen Code ein !!!
   */
-});
+};
